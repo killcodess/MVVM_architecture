e5c2e952f28c67c0d02e2461db63fdf12d2682a8
chandan.mohapatra <chandanmohapatra324@gmail.com>
2023-08-06 20:14:45 +0530

    changes in apiinterface

--- app/src/main/java/com/example/mvvmdemo/api/ApiInterface.kt
+++ app/src/main/java/com/example/mvvmdemo/api/ApiInterface.kt
@@ -1,11 +1,16 @@
 package com.example.mvvmdemo.api
 
 import com.example.mvvmdemo.model.User
+import com.google.gson.JsonObject
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiInterface {
 
     @GET("/43ea2ba0-22cf-4a04-ba1b-e62e88aaaa52")
     suspend fun getUserData() : Response<User>
+
+    @POST("43ea2ba0-22cf-4a04-ba1b-e62e88aaaa52")
+    suspend fun getUserDataByPostingRequestBody(jsonObject: JsonObject) : Response<User>
 }
